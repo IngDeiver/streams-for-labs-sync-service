5b5d36c73e843d877be62e4feff6ae2694672b22
Deiver Carrascal <ingendeiver@gmail.com>
2021-04-14 08:40:10 -0500

    fix: vault aes uri

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { createServer } = require('sync-files-cipher')
 const { queryVault } = require('./apivault')
-const AES_VAULT_URI = process.env.AES_VAULT_URI | ''
+
 
 async function startServer (){
     try {
+        const AES_VAULT_URI = process.env.AES_VAULT_URI
+        console.log("AES_VAULT_URI: ", AES_VAULT_URI);
         const  keys  = await queryVault(AES_VAULT_URI)
         createServer(
             keys.key,
